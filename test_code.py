c4bee1e5ed322cd9272bd9ed341d0b88be940c62
tsinghong <xxjj1205@163.com>
2021-02-18 10:36:14 +0800

    0218change

--- test_code.py
+++ test_code.py
@@ -125,89 +125,16 @@
 # 学院类：开设哪些课程，主要是专业学科
 # 学生类：选修哪些具体课程，必修是固定的，无法选择
 
-class college:
-    def __init__(self, class_num):
-        self.class_num = class_num
-        self.academy = []
-    
-    def academy_manipulate(self):
-        while True:
-            choice_input = input("""
-            请输入需要的操作：
-            1. 增加学院
-            2. 删除学院
-            0. 退出
-            """)
-            if choice_input = '1':
-                academy_name = input(""" 
-                请输入学院名：
-                """)
-                self.academy.append(academy_name)
-                if academy_name in self.academy:
-                    print("添加成功！")
-                else:
-                    print("添加失败，请重新添加！")
-                    # continue
-            elif choice_input = '2':
-                pass
-            elif choice_input = '0':
-                break
-            else:
-                print("输入的编号错误，请重新输入！")
-                continue
-        
-        
-
-    def tell_info(self, choice):
-        while True:
-            choice_input = input(""" 
-            请选择要查看的信息：
-            1. 查看班级数
-            2. 查看公共科目
-            0. 退出
-            """)
-            if choice_input = '1'
-                choice_input = input(
-                """
-                请选择要查看的年级:
-                1. 大一
-                2. 大二
-                3. 大三
-                4. 大四
-                0. 退出
-                """)
-                # 好像可以用switch语句
-                if choice_input == '1':
-                    print(f"大一有{self.class_num}班级")
-                elif choice_input == '2':
-                    print(f"大二有{self.class_num}班级")
-                elif choice_input == '3':
-                    print(f"大三有{self.class_num}班级")
-                elif choice_input == '4':
-                    print(f"大四有{self.class_num}班级")
-                elif choice_input == '0':
-                    print("已退出")
-                    break
-                else:
-                    print("输入的数字有误，请重新输入！")
-                    continue
-            elif choice_input = '2':
-                if self.academy:
-                    for acad in self.academy:
-                        print(acad)
-                else:
-                    print("尚未设置学院！")
-            elif choice_input = '0':
-                break
-            else:
-                print("输入的编号有误，请重新输入！")
-                continue
-
-college_1 = college()
 
 
-
-
-
+dic1 = {"name": "xiaoming", "age": 27}
+dic2 = {"gender": "male"}
+dic1.update(dic2)
+print(dic1)
+# dic2["gender"] = "female"
+print(dic1.get("gender"))
+print(dic2.get("gender"))
+print(id(dic1.get("gender")))
+print(id(dic2.get("gender")))
 
 
