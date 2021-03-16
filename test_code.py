32f18b608ef228f21443d46466a522a57b8f94b0
tsinghong <xxjj205@163.com>
2021-03-16 15:31:34 +0800

    0316-15:31pm

--- test_code.py
+++ test_code.py
@@ -1,15 +1,30 @@
-a = 1
-b = 2
-c = a + b
-print(c)
+# a = 1
+# b = 2
+# c = a + b
+# print(c)
+
+# msg = "helloworld"
+# print(msg[:5:3])
 
 
+# msg = 'abcaaaadefga'
+# print(msg.strip('a'))
+
+# msg = 'abcdefga'
+# print(msg.strip('a'))
 
 
+# info='lilei 18 male'
+# res=info.split()
+# print(res) 
 
 
+# info='egon:18:male'
+# for x in info:
+#     print(x)
 
 
-
-
+print(tuple('hello'))
+print(tuple([1,2,3]))
+print(tuple({'a1':111,'a2':333}))
 
